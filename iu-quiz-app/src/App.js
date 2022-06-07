3062515cd86003adb2e056482c1b6ef150451fef
Anna Wolf <anna@bannerwave.com>
2022-06-07 19:27:55 +0200

    Apollo setup

--- iu-quiz-app/src/App.js
+++ iu-quiz-app/src/App.js
@@ -4,12 +4,29 @@ import theme from './styles/theme';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalContext } from './context/ModalContext'
 import { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 
 import Navigation from './components/blocks/Navigation'
 import Footer from './components/blocks/Footer';
 import Pages from './pages/Pages';
 import Modal from './components/modals/Modal'
+
+const client = new ApolloClient({
+  /*
+  uri: 'https://api-wbplatform-staging.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+  defaultOptions: {
+      watchQuery: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all'
+      },
+      query: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all'
+      }
+  }*/
+})
 
 
 
@@ -18,17 +35,19 @@ function App() {
   const [open, setOpen] = useState(false)
   const [type, setType] = useState("")
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <ModalContext.Provider value={{open, setOpen, type, setType}}>
-        <BrowserRouter>
-          <Modal type="NewGame" />
-          <Navigation />
-          <Pages />
-          <Footer />
-        </BrowserRouter>
-      </ModalContext.Provider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <ModalContext.Provider value={{open, setOpen, type, setType}}>
+          <BrowserRouter>
+            <Modal type="NewGame" />
+            <Navigation />
+            <Pages />
+            <Footer />
+          </BrowserRouter>
+        </ModalContext.Provider>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
