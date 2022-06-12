89e378f433c26cdbb56b44060055748429eacc7e
Anna Wolf <anna@bannerwave.com>
2022-06-12 09:30:57 +0200

    Datenschutz Content

--- iu-quiz-app/src/App.js
+++ iu-quiz-app/src/App.js
@@ -4,7 +4,7 @@ import theme from './styles/theme';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalContext } from './context/ModalContext'
 import { useState } from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
 
 
 import Navigation from './components/blocks/Navigation'
@@ -12,30 +12,13 @@ import Footer from './components/blocks/Footer';
 import Pages from './pages/Pages';
 import Modal from './components/modals/Modal'
 
-const client = new ApolloClient({
-  /*
-  uri: 'https://api-wbplatform-staging.herokuapp.com/graphql',
-  cache: new InMemoryCache(),
-  defaultOptions: {
-      watchQuery: {
-          fetchPolicy: 'no-cache',
-          errorPolicy: 'all'
-      },
-      query: {
-          fetchPolicy: 'no-cache',
-          errorPolicy: 'all'
-      }
-  }*/
-})
-
-
 
 
 function App() {
   const [open, setOpen] = useState(false)
   const [type, setType] = useState("")
   return (
-    <ApolloProvider client={client}>
+
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <ModalContext.Provider value={{open, setOpen, type, setType}}>
@@ -47,7 +30,6 @@ function App() {
           </BrowserRouter>
         </ModalContext.Provider>
       </ThemeProvider>
-    </ApolloProvider>
   );
 }
 
